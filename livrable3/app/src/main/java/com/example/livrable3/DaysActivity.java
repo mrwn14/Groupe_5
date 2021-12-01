20a1662840b86ac809a7d23d3a0c8af12d663ed4
Tawfiq <abubakertawfiq2@gmail.com>
2021-12-01 03:13:06 -0500

    Added clock and some employee functionality

---
+++ livrable3/app/src/main/java/com/example/livrable3/DaysActivity.java
@@ -0,0 +1,14 @@
+package com.example.livrable3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DaysActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_days);
+    }
+}
